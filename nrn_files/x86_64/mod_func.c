99404ba83cf5e1bc82a22dad059b79451dc3fbf7
yzerlaut <zerlaut@unic.cnrs-gif.fr>
2014-01-22 11:31:39 +0100

    adding my netstim

--- nrn_files/x86_64/mod_func.c
+++ nrn_files/x86_64/mod_func.c
@@ -7,11 +7,17 @@ modl_reg(){
     fprintf(stderr, "Additional mechanisms from files\n");
 
     fprintf(stderr," Exp2Syn_eiNtwk.mod");
+    fprintf(stderr," hh2.mod");
+    fprintf(stderr," instantaneous_freq.mod");
     fprintf(stderr," myIClamp.mod");
     fprintf(stderr," myVClamp.mod");
+    fprintf(stderr," netstim2.mod");
     fprintf(stderr, "\n");
   }
   _Exp2Syn_eiNtwk_reg();
+  _hh2_reg();
+  _instantaneous_freq_reg();
   _myIClamp_reg();
   _myVClamp_reg();
+  _netstim2_reg();
 }

b9eb67742e0cb906198fedda7f4025cd0db28231
yzerlaut <zerlaut@unic.cnrs-gif.fr>
2014-01-31 16:51:29 +0100

    analys

--- analysis/elphy_to_python.py
+++ analysis/elphy_to_python.py
@@ -1,23 +1,26 @@
 from neo.io import ElphyIO
 import numpy as np
 import sys
+import json
 
-File = str(sys.argv[1])
-reader = ElphyIO(filename=File)
+def get_analogsignals(filename):
+    File = str(filename)
+    reader = ElphyIO(filename=File)
+    seg = reader.read_block(lazy=False, cascade=True)
+    sampling_freq = seg.segments[0].analogsignals[0].sampling_rate.magnitude.tolist()
+    tstart = seg.segments[0].analogsignals[0].t_start.magnitude.tolist()
+    tstop = seg.segments[0].analogsignals[0].t_stop.magnitude.tolist()
+    print sampling_freq, tstart, tstop
 
+    t = np.arange(tstart, tstop, 1./sampling_freq)
+    data = np.array(seg.segments[0].analogsignals, dtype='float32')
+    DATA = []
+    DATA.append(t)
+    for d in data:
+        DATA.append(d)
+    return data
 
-seg = reader.read_block(lazy=False, cascade=True)
-
-sampling_freq = seg.segments[0].analogsignals[0].sampling_rate.magnitude.tolist()
-tstart = seg.segments[0].analogsignals[0].t_start.magnitude.tolist()
-tstop = seg.segments[0].analogsignals[0].t_stop.magnitude.tolist()
-print sampling_freq, tstart, tstop
-
-t = np.arange(tstart, tstop, 1./sampling_freq)
-data = np.array(seg.segments[0].analogsignals, dtype='float32')
-
-DATA = []
-DATA.append(t)
-for d in data:
-    DATA.append(d)
-    
+def get_metadata(filename):
+    with file(filename) as fid:
+        params = json.load(fid)
+    return params

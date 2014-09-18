6abd4ede80bf432f394325d5d108f5e70db08f11
yzerlaut <zerlaut@unic.cnrs-gif.fr>
2014-09-18 17:06:03 +0200

    up lab

--- nrn_files/mod_func.c
+++ nrn_files/mod_func.c
@@ -12,16 +12,24 @@ modl_reg(){
 
 fprintf(stderr," Exp2Syn_eiNtwk.mod");
 fprintf(stderr," IClamp_WhiteNoise.mod");
+fprintf(stderr," WangBuzsaki.mod");
+fprintf(stderr," adexp.mod");
 fprintf(stderr," hh2.mod");
+fprintf(stderr," instantaneous_freq.mod");
 fprintf(stderr," myIClamp.mod");
 fprintf(stderr," myVClamp.mod");
 fprintf(stderr," netstim_from_self_spike.mod");
+fprintf(stderr," syn_population_stim.mod");
 fprintf(stderr, "\n");
     }
 _Exp2Syn_eiNtwk_reg();
 _IClamp_WhiteNoise_reg();
+_WangBuzsaki_reg();
+_adexp_reg();
 _hh2_reg();
+_instantaneous_freq_reg();
 _myIClamp_reg();
 _myVClamp_reg();
 _netstim_from_self_spike_reg();
+_syn_population_stim_reg();
 }
